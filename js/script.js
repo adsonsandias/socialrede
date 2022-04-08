6fa9be2f0963cb34c0463996c9846158e819df37
adsonbmx15 <adsonbmx18@gmail.com>
2022-04-07 23:31:35 -0300

    add-darkmode-add-js-menu-add-effects-ajust

--- js/script.js
+++ js/script.js
@@ -4,3 +4,33 @@ function activeLink() {
   this.classList.add("active-sidenav");
 }
 sidenav.forEach((item) => item.addEventListener("click", activeLink));
+
+const bntCurti = document.querySelectorAll(".nav-publicacao li a");
+function activeCurti() {
+  bntCurti.forEach((item) => item.classList.remove("btn-p-ative"));
+  this.classList.add("btn-p-ative");
+}
+bntCurti.forEach((item) => item.addEventListener("click", activeCurti));
+
+const bntModo = document.querySelectorAll(".modo-noturno a");
+function activeModo() {
+  bntModo.forEach((item) => item.classList.remove("on-modo"));
+  this.classList.add("on-modo");
+}
+bntModo.forEach((item) => item.addEventListener("click", activeModo));
+
+const choseBtn = document.querySelector(".close");
+const sideNav = document.querySelector(".sidenav");
+const menu = document.querySelector(".menu");
+
+choseBtn.addEventListener("click", () => {
+  sideNav.style.display = "none";
+  // menu.style.display = "flex";
+  // choseBtn.style.display = "none";
+});
+
+menu.addEventListener("click", () => {
+  sideNav.style.display = "flex";
+  // menu.style.display = "none";
+  // choseBtn.style.display = "flex";
+});
