9312ae0f58d625deb8b062f672879063cfa0a5a6
adsonbmx15 <adsonbmx18@gmail.com>
2022-04-08 16:06:25 -0300

    add-menuJS-addMobileAjust-add-sidenavAjust

--- js/script.js
+++ js/script.js
@@ -12,12 +12,13 @@ function activeCurti() {
 }
 bntCurti.forEach((item) => item.addEventListener("click", activeCurti));
 
-const bntModo = document.querySelectorAll(".modo-noturno a");
-function activeModo() {
-  bntModo.forEach((item) => item.classList.remove("on-modo"));
-  this.classList.add("on-modo");
-}
-bntModo.forEach((item) => item.addEventListener("click", activeModo));
+// const bntModo = document.querySelectorAll(".modo-noturno a");
+// function activeModo() {
+//   bntModo.forEach((item) => item.classList.remove("on-modo"));
+//   this.classList.add("on-modo");
+//   document.body.classList.add("light");
+// }
+// bntModo.forEach((item) => item.addEventListener("click", activeModo));
 
 const choseBtn = document.querySelector(".close");
 const sideNav = document.querySelector(".sidenav");
@@ -34,3 +35,31 @@ menu.addEventListener("click", () => {
   // menu.style.display = "none";
   // choseBtn.style.display = "flex";
 });
+
+const bntModoLight = document.querySelectorAll(".modo-noturno a.light");
+const bntModoDark = document.querySelectorAll(".modo-noturno a.dark");
+const logoDark = document.querySelectorAll(".dark-logo");
+const logoLight = document.querySelectorAll(".light-logo");
+const placeHoder = document.querySelector("input#pesquisa::placeholder");
+
+function activeModoLight() {
+  bntModoLight.forEach((item) => item.classList.remove("on-modo"));
+  this.classList.add("on-modo");
+  document.body.classList.add("light");
+  document.body.classList.remove("dark");
+  bntModoDark.forEach((item) => item.classList.remove("on-modo"));
+  logoDark.forEach((item) => (item.style.display = "none"));
+  logoLight.forEach((item) => (item.style.display = "block"));
+}
+bntModoLight.forEach((item) => item.addEventListener("click", activeModoLight));
+
+function activeModoDark() {
+  bntModoDark.forEach((item) => item.classList.remove("on-modo"));
+  this.classList.add("on-modo");
+  document.body.classList.add("dark");
+  document.body.classList.remove("light");
+  bntModoLight.forEach((item) => item.classList.remove("on-modo"));
+  logoDark.forEach((item) => (item.style.display = "block"));
+  logoLight.forEach((item) => (item.style.display = "none"));
+}
+bntModoDark.forEach((item) => item.addEventListener("click", activeModoDark));
